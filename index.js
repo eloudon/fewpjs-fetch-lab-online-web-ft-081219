ef0c8c7b14bf4164e3f2739757dac7942fc906af
Eric Loudon <loudon.eric@gmail.com>
2019-11-29 16:26:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 function fetchBooks() {
+  let allBooks = ''
+
   fetch("https://anapioficeandfire.com/api/books")
-  .then(resp => resp.json())
-  .then (json => renderBooks(json))
+      .then(res => res.json())
+      .then(books => {
+      allBooks = books
+   })
+ }
+
+function renderBooks(allBooks ) {
 
 }
 
 function renderBooks(json) {
   const main = document.querySelector('main')
-  json.forEach(book => {
+  allBooks.forEach(book => {
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
